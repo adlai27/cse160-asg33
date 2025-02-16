5af5843c9f6e094d23ad95148327ba467b8310cf
adlai27 <167170841+adlai27@users.noreply.github.com>
2025-02-15 18:22:10 -0800

    Update BlockyCube.js

--- BlockyCube.js
+++ BlockyCube.js
@@ -3,11 +3,7 @@ class BlockyCube {
   constructor() {
     this.color = [1,1,1,1];
     this.matrix = new Matrix4();
-    this.textureNum = -2;  // -2 => color-only by default
-
-    // A 1×1×1 cube, centered at the origin (coordinates from -0.5..+0.5).
-    // We'll store 36 vertices (12 triangles) in a single Float32Array.
-    // Front, Back, Left, Right, Top, Bottom faces.
+    this.textureNum = -2;  
 
     this.vertices = [
       // front face (2 triangles)
@@ -35,8 +31,6 @@ class BlockyCube {
       -0.5,-0.5,-0.5,   0.5,-0.5, 0.5,  -0.5,-0.5, 0.5,
     ];
 
-    // Simple UVs for each triangle. Here we just do a basic 0..1 mapping per face.
-    // It's up to you how you lay out the texture.
     this.uvs = [
       // front face
       0,0, 1,1, 1,0,
@@ -60,7 +54,6 @@ class BlockyCube {
   }
 
   render() {
-    // Set uniform color, texture, transform
     gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
     gl.uniform1i(u_whichTexture, this.textureNum);
     gl.uniform4f(u_FragColor, this.color[0], this.color[1], this.color[2], this.color[3]);
