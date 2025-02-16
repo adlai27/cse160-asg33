c8d6dce3c999bf506158037a70746d7608a64edd
adlai27 <167170841+adlai27@users.noreply.github.com>
2025-02-15 18:22:30 -0800

    Update BlockyPyramid.js

--- BlockyPyramid.js
+++ BlockyPyramid.js
@@ -3,11 +3,8 @@ class BlockyPyramid {
   constructor() {
     this.color = [1,1,1,1];
     this.matrix = new Matrix4();
-    this.textureNum = -2; // default color-only
+    this.textureNum = -2;
 
-    // We'll build a square-based pyramid:
-    // base at y=0, corners at (±0.5,0,±0.5), apex at (0,1,0).
-    // 6 triangles total: 2 for base, then 4 sides.
 
     this.vertices = [
       // base (2 triangles)
@@ -24,8 +21,7 @@ class BlockyPyramid {
       -0.5,0, 0.5,  -0.5,0,-0.5,   0,1,0,
     ];
 
-    // Basic UV mapping. For the base, we map corners in [0..1]. 
-    // For the sides, we'll do something simple, e.g. all 0..1 as well.
+
     this.uvs = [
       // base (2 triangles)
       0,0, 1,0, 1,1,
